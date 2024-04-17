0ddcba0a4e437241a5df5e3d091762c93e3ea8ae
Appanavenkatasaicharan <109053463+Appanavenkatasaicharan@users.noreply.github.com>
2024-04-17 23:11:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ const { Console } = require('console');
 
 var app = express();
 
+app.use(cors())
+
 app.use(rateLimiter({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
