35e47eef4393fec5cb291f6b847409c16c1360f3
Appanavenkatasaicharan <109053463+Appanavenkatasaicharan@users.noreply.github.com>
2024-04-17 15:53:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ const { Console } = require('console');
 
 var app = express();
 
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
+
 app.use(cors())
 
 app.set('trust proxy', 1)
